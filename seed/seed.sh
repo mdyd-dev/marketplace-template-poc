75a749b7de6d15ef013f0fe3872b3d4b5a7aec61
dariusz gorzeba <dgorzeba@gmail.com>
2021-01-27 11:10:12 +0100

    update seed generator script

--- seed/seed.sh
+++ seed/seed.sh
@@ -4,6 +4,10 @@ cd seed
 
 ruby convert.rb
 
+cd images
+zip -r ../images.zip sample_item_photo/ sample_profile_photo
+cd -
+
 zip data models.csv users.csv
 cp categories-i18n.en.txt ../app/translations/en_categories.yml
 
@@ -12,4 +16,4 @@ cd ..
 pos-cli data clean --auto-confirm --include-schema
 pos-cli deploy
 pos-cli data import --path=./seed/data.zip --zip
-pos-cli uploads upload --path=seed/images
+pos-cli uploads push --path=seed/images.zip
